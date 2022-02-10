7573785d814f848db5a7aae885370943381bfdb5
khaleel-lab <kbasha968@gmail.com>
2022-02-10 19:03:13 +0530

    MongoDB Connection, Created Model

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -16,5 +20,5 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 app.use(errorHandler);
 
 app.listen(port, () => {
-	console.log(`Server running on ${port}`);
+	console.log(`Server running on port ${port}`.yellow.underline);
 });
